ecd84034dbdabfe44aff9b219659cec93743ed3e
DESKTOP-HO7V4OL\daffodil-31 <manish.jha@daffodilsw.com>
2018-09-24 11:23:47 +0530

    added new comment in master local

--- app/src/main/java/com/example/daffodil_31/vcsdemo/MainActivity.java
+++ app/src/main/java/com/example/daffodil_31/vcsdemo/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
         MyClass myClass = new MyClass(10);
         //this comment is written in local only
+        //this comment is added only in local
 
 
 
